76360b20c4670cf312baca392a5463445275dd6f
Mai <garcia.ton94@gmail.com>
2023-02-09 20:37:00 -0500

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+let messages = [];
+function writeCards(name, event) {
+    for (let i = 0; i < name.length; i++) {
+        const message = `Thank you, ${name[i]}, for the wonderful ${event} gift!`;
+    messages.push(message); 
+}
+    return messages;
+}
+
+function countDown(number) {
+    while (number > 0) {
+        console.log(number);
+        number--;
+        }
+        console.log(number);
+    }
+
+countDown(10);
+
