7ff0a26743e5044a518aac3996f3288d642bb0b2
Mai <garcia.ton94@gmail.com>
2023-05-02 13:22:56 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let messages = [];
+/* let messages = [];
 function writeCards(name, event) {
     for (let i = 0; i < name.length; i++) {
         const message = `Thank you, ${name[i]}, for the wonderful ${event} gift!`;
@@ -16,4 +16,24 @@ function countDown(number) {
     }
 
 countDown(10);
+ */
 
+
+let messages = [];
+
+function writeCards(name, event) {
+    for (let i = 0; i < name.length; i++) {
+        const message = `Thank you, ${name[i]}, for the wonderful ${event} gift!`
+        messages.push(message);
+    }
+    return messages;}
+
+function countDown(number) {
+    while (number>0) {
+        console.log(number);
+        number--;
+    }
+    console.log(number);
+}
+
+countDown(10);
